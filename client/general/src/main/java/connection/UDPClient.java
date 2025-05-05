5229d492e41dcce9c10884a2e2251e7542992fef
lis <liza850l@yandex.ru>
2025-05-05 17:56:26 +0300

    Сделан client

---
+++ client/general/src/main/java/connection/UDPClient.java
@@ -0,0 +1,35 @@
+package connection;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UDPClient{
+  public final static int SERVICE_PORT = 54021;
+  
+  public static byte[] sendAndReceive(byte[] sendingData) {
+    try (DatagramSocket clientSocket = new DatagramSocket();){
+      
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+      byte[] receivingDataBuffer = new byte[65500];
+      
+      DatagramPacket sendingPacket = new DatagramPacket(sendingData,sendingData.length,IPAddress, SERVICE_PORT);
+      clientSocket.send(sendingPacket);
+
+      DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer,receivingDataBuffer.length);
+      clientSocket.receive(receivingPacket);
+      
+      byte[] receivedData = receivingPacket.getData();
+
+      return receivedData;
+    }
+    catch(SocketException e) {
+      System.out.println("Нет соединения.");
+      System.out.println(e.getMessage());
+    } catch (IOException e) {
+      System.out.println("Ошибка передачи данных.");
+    }
+    return null;
+  }
+}
